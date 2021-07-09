6c2a6330999e7f0c92171b4eaad0b14293432097
tarun2506 <tarunsharma8920@gmail.com>
2021-07-09 15:15:52 +0530

    added about section on aboutus page

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+// Pages components
+import AboutSection from "../components/AboutSection";
+
+const AboutUs = () => {
+  return <AboutSection />;
+};
+
+export default AboutUs;
