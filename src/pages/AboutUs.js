f72e253b33e5e8709f6767afbc403648e912d4fd
tarun2506 <tarunsharma8920@gmail.com>
2021-07-09 15:52:26 +0530

    Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 // Pages components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
   return (
     <div>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </div>
   );
 };
