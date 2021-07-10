798f437f11faf5d1ce869cfac93431915fd740da
tarun2506 <tarunsharma8920@gmail.com>
2021-07-10 16:26:59 +0530

    React router

--- src/App.js
+++ src/App.js
@@ -3,14 +3,28 @@ import React from "react";
 import GlobalStyle from "./components/GlobalStyle";
 // Import Pages:
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import MyWork from "./pages/MyWork";
 import Nav from "./components/Nav";
+
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path="/">
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <MyWork />
+        </Route>
+        <Route path="/contact">
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
