d565de828c874f168305bb7a7d1824208b4a320b
tarun2506 <tarunsharma8920@gmail.com>
2021-07-10 21:40:27 +0530

    added framer motion 01

--- src/App.js
+++ src/App.js
@@ -7,28 +7,31 @@ import ContactUs from "./pages/ContactUs";
 import MyWork from "./pages/MyWork";
 import Nav from "./components/Nav";
 import MovieDetails from "./pages/MovieDetails";
-
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route exact path="/">
-          <AboutUs />
-        </Route>
-        <Route exact path="/work">
-          <MyWork />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetails />
-        </Route>
-      </Switch>
+      <AnimatePresence exitBeforeEnter>
+        <Switch location={location} key={location.pathname}>
+          <Route exact path="/">
+            <AboutUs />
+          </Route>
+          <Route exact path="/work">
+            <MyWork />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetails />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
 import { MovieState } from "../movieState";
+// Animation
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 const MovieDetails = () => {
   const history = useHistory();
@@ -19,7 +22,12 @@ const MovieDetails = () => {
   return (
     <>
       {movie && (
-        <Details>
+        <Details
+          variants={pageAnimation}
+          initial="hidden"
+          animate="show"
+          exit="exit"
+        >
           <Headline>
             <h2>{movie.title}</h2>
             <img src={movie.mainImg} alt="movieImg" />
@@ -42,7 +50,7 @@ const MovieDetails = () => {
   );
 };
 
-const Details = styled.div`
+const Details = styled(motion.div)`
   color: white;
 `;
 const Headline = styled.div`
