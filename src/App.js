8cd8d0d4872f9dd64f07a6440396898d5a370010
tarun2506 <tarunsharma8920@gmail.com>
2021-07-10 18:13:17 +0530

    added films

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import MyWork from "./pages/MyWork";
 import Nav from "./components/Nav";
+import MovieDetails from "./pages/MovieDetails";
 
 import { Route, Switch } from "react-router-dom";
 
@@ -18,11 +19,14 @@ function App() {
         <Route exact path="/">
           <AboutUs />
         </Route>
-        <Route path="/work">
+        <Route exact path="/work">
           <MyWork />
         </Route>
         <Route path="/contact">
           <ContactUs />
+        </Route>
+        <Route path="/work/:id">
+          <MovieDetails />
         </Route>
       </Switch>
     </div>
