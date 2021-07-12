ec31ab2a4b18fa4c777fd8ba4b44fb5fe885ab36
tarun2506 <tarunsharma8920@gmail.com>
2021-07-12 17:54:12 +0530

    page transitions

--- src/animation.js
+++ src/animation.js
@@ -7,14 +7,13 @@ export const pageAnimation = {
     opacity: 1,
     y: 0,
     transition: {
-      duration: 1,
+      duration: 0.5,
       when: "beforeChildren",
       staggerChildren: 0.25,
     },
   },
   exit: {
     opacity: 0,
-    y: 300,
     transition: {
       duration: 0.5,
     },
@@ -36,6 +35,30 @@ export const photoAnim = {
   show: {
     scale: 1,
     opacity: 1,
-    transition: { duration: 0.6, ease: "easeInOut" },
+    transition: { duration: 0.8, ease: "easeInOut" },
+  },
+};
+
+export const lineAnim = {
+  hidden: { width: "0%" },
+  show: { width: "100%", transition: { duration: 1 } },
+};
+
+export const slider = {
+  hidden: { x: "130%", skew: "-45deg" },
+  show: {
+    x: "-100%",
+    skew: "0deg",
+    transition: { ease: "easeOut", duration: 1 },
+  },
+};
+
+export const sliderContainer = {
+  hidden: {
+    opacity: 1,
+  },
+  show: {
+    opacity: 1,
+    transition: { staggerChildren: 0.15, ease: "easeInOut" },
   },
 };
